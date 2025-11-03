bf185afc7ae00901b5bd571bd28b4f60cc39450b
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-11-03 13:52:11 +0500

    Task2

---
+++ Tyuiu.AtanaevRI.Sprint4.Task2.V20.Test/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AtanaevRI.Sprint4.Task2.V20.Lib;
+namespace Tyuiu.AtanaevRI.Sprint4.Task2.V20.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] array = { 8, 4, 7, 7, 3, 8, 8, 5, 8, 7, 2, 4 };
+            DataService ds = new DataService();
+            int res = ds.Calculate(array);
+            Assert.AreEqual(29, res);
+        }
+    }
+}
---
+++ Tyuiu.AtanaevRI.Sprint4.Task2.V20/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.AtanaevRI.Sprint4.Task2.V20.Lib;
+DataService ds = new DataService();
+Random rnd = new Random();
+
+
+
+
+
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+int len;
+Console.WriteLine("*        Введите количество элементов массива                                   *");
+len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];
+
+for (int i = 0; i < len; i++)
+{
+    array[i] = rnd.Next(2,7);
+}
+
+Console.WriteLine("Массив");
+for (int i = 0; i < len; i++)
+{
+    Console.Write(array[i] );
+}
+
+
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int res = ds.Calculate(array);
+Console.WriteLine(res);
